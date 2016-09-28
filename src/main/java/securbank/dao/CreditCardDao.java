0d74abef7cc826c9d2ca522360579721dc6b1e0c
Shivani Jhunjhunwala <shivani.jhunjhunwala@yahoo.com>
2016-09-27 20:43:57 -0700

    credit card dao

---
+++ src/main/java/securbank/dao/CreditCardDao.java
@@ -0,0 +1,14 @@
+package securbank.dao;
+
+import java.util.List;
+import java.util.UUID;
+
+import securbank.models.CreditCard;
+
+public interface CreditCardDao extends BaseDao<CreditCard, UUID> {
+
+	public List<CreditCard> findAll();
+	public CreditCard findByCcId(UUID id);
+	public CreditCard findByAccountNo(UUID accountID);
+	
+}
---
+++ src/main/java/securbank/dao/CreditCardDaoImpl.java
@@ -0,0 +1,56 @@
+package securbank.dao;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import securbank.models.CreditCard;
+
+@Repository("creditCardDao")
+public class CreditCardDaoImpl extends BaseDaoImpl<CreditCard, UUID> implements CreditCardDao {
+	@Autowired
+	EntityManager entityManager;
+	
+	public CreditCardDaoImpl() {
+		super(CreditCard.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<CreditCard> findAll() {
+		return (List<CreditCard>) this.entityManager.createQuery("SELECT creditcard from CreditCard creditcard")
+				.getResultList();
+	
+	}
+
+	@Override
+	public CreditCard findByAccountNo(UUID accountID) {
+		try {
+			return (CreditCard) this.entityManager.createQuery("SELECT creditcard from CreditCard creditcard where (creditcard.accountID = :accountID) AND creditcard.active = 1")
+					.setParameter("accountID", accountID)
+					.getSingleResult();
+		}
+		catch(NoResultException e) {
+			// returns null if no user if found
+			return null;
+		}
+	}
+
+	@Override
+	public CreditCard findByCcId(UUID id) {
+		try {
+			return (CreditCard) this.entityManager.createQuery("SELECT creditcard from CreditCard creditcard where (creditcard.ccID = :ccID) AND creditcard.active = 1")
+					.setParameter("ccID", id)
+					.getSingleResult();
+		}
+		catch(NoResultException e) {
+			// returns null if no user if found
+			return null;
+		}
+	}
+}
