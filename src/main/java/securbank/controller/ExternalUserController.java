e073e8b73027236052f5f10c170a724502b7311c
ayush2k <ayush2k@gmail.com>
2016-10-02 03:38:26 -0700

    adds routing for requests

--- src/main/java/securbank/controller/ExternalUserController.java
+++ src/main/java/securbank/controller/ExternalUserController.java
@@ -11,9 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import securbank.models.ModificationRequest;
 import securbank.models.User;
 import securbank.services.UserService;
+import securbank.validators.EditUserFormValidator;
 import securbank.validators.NewUserFormValidator;
+
 /**
  * @author Ayush Gupta
  *
@@ -25,6 +28,9 @@ public class ExternalUserController {
 	
 	@Autowired 
 	NewUserFormValidator userFormValidator;
+	
+	@Autowired 
+	EditUserFormValidator editUserFormValidator;
 	
 	@GetMapping("/signup")
     public String signupForm(Model model) {
@@ -45,10 +51,31 @@ public class ExternalUserController {
         return "redirect:/";
     }
 	
+	@GetMapping("/user/edit")
+    public String currentUserEdit(Model model) {
+		model.addAttribute(userService.getCurrentUser());
+		
+        return "edit";
+    }
+	
+	@PostMapping("/edit")
+    public String editSubmit(@ModelAttribute ModificationRequest request, BindingResult bindingResult) {
+		editUserFormValidator.validate(request, bindingResult);
+		if (bindingResult.hasErrors()) {
+			return "edit";
+        }
+		
+		// create request
+    	userService.createModificationRequest(request);
+    	
+        return "redirect:/";
+    }
+	
 	@GetMapping("/user/details")
     public String currentUserDetails(Model model) {
 		model.addAttribute(userService.getCurrentUser());
 		
         return "details";
     }
+	
 }
