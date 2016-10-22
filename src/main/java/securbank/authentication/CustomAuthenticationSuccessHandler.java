9da5398a77506bf2f7cfdf2a72519392e42bd8ae
Nikh13 <nikhil.loney@gmail.com>
2016-10-22 15:08:19 -0700

    minor code refactoring

--- src/main/java/securbank/authentication/CustomAuthenticationSuccessHandler.java
+++ src/main/java/securbank/authentication/CustomAuthenticationSuccessHandler.java
@@ -3,31 +3,31 @@ package securbank.authentication;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import securbank.services.AuthenticationService;
+
 @Component
 public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+	@Autowired
+	private AuthenticationService authService;
+	
 	@Override
     protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response) {
         // Get the role of logged in user
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String role = auth.getAuthorities().toString();
-
-        String targetUrl = "";
-        if(role.contains("ADMIN")) {
-            targetUrl = "/admin/details";
-        } else if(role.contains("MANAGER")) {
-            targetUrl = "/manager/details";
-        } else if(role.contains("EMPLOYEE")) {
-            targetUrl = "/manager/details";
-        } else if(role.contains("INDIVIDUAL")|role.contains("MERCHANT")) {
-            targetUrl = "/external/details";
+        
+        if(role==null){
+        	return "/";
         }
-        return targetUrl;
+
+        return authService.getRedirectUrlFromRole(role);
     }
 
 }
--- src/main/java/securbank/config/WebSecurityConfig.java
+++ src/main/java/securbank/config/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package securbank.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -70,5 +72,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationTrustResolver trustResolver() {
+        return new AuthenticationTrustResolverImpl();
     }
 }
