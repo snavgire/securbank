703d8059f28723f4fe8860aab1508e47c44781e5
Nikh13 <nikhil.loney@gmail.com>
2016-10-25 20:25:47 -0700

    initial success handling for merchant and external user

--- src/main/java/securbank/controller/MerchantController.java
+++ src/main/java/securbank/controller/MerchantController.java
@@ -138,7 +138,7 @@ public class MerchantController {
 		//deactivate current otp
 		otpService.deactivateOtpByUser(userService.getCurrentUser());
 		
-		return "redirect:/merchant/createtransaction";
+		return "redirect:/merchant/createtransaction?successTransaction=true";
     }
 	
 	@GetMapping("/merchant/createtransfer")
@@ -177,7 +177,7 @@ public class MerchantController {
 		//deactivate current otp
 		otpService.deactivateOtpByUser(userService.getCurrentUser());
 		
-		return "redirect:/merchant/createtransfer";
+		return "redirect:/merchant/createtransfer?successTransaction=true";
 	}
 
 	@GetMapping("/merchant/payment")
@@ -222,7 +222,7 @@ public class MerchantController {
 		//deactivate current otp
 		otpService.deactivateOtpByUser(userService.getCurrentUser());
 		
-		return "redirect:/merchant/payment";
+		return "redirect:/merchant/payment?successPayment=true";
 	}
 	
 	@GetMapping("/merchant/edit")
@@ -246,7 +246,7 @@ public class MerchantController {
 		// create request
     	userService.createExternalModificationRequest(user);
 	
-        return "redirect:/merchant/details";
+        return "redirect:/merchant/details?successEdit=true";
     }
 	
 
@@ -296,7 +296,7 @@ public class MerchantController {
 		
 		logger.info("GET request: Manager approve/decline external transaction requests");
 		
-        return "redirect:/merchant/transfers";
+        return "redirect:/merchant/transfers?successAction=true";
     }
 	
 	@GetMapping("/merchant/transfer/{id}")
@@ -380,7 +380,7 @@ public class MerchantController {
 		authorization.setStatus(status);
 		authorization = viewAuthorizationService.approveAuthorization(authorization);
 		
-        return "redirect:/merchant/request";
+        return "redirect:/merchant/request?successAction=true";
     }
 
 }
